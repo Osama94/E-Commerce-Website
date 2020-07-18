f5988547b489e417494e9df63c8dfb0383b3e311
osama ahmed <oahmed@e-jawdah.com>
2020-07-18 10:42:06 +0200

    Finish Order Details View Client Side

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -12,12 +12,17 @@ const routes: Routes = [
   { path: 'server-error', component: ServerErrorComponent, data: { breadcrumb: 'Server Error' } },
   { path: 'not-found', component: NotFoundComponent, data: { breadcrumb: 'Not Found' } },
   { path: 'shop', loadChildren: () => import('./shop/shop.module').then(mod => mod.ShopModule), data: { breadcrumb: 'Shop' }},
-  {path: 'basket', loadChildren: () => import('./basket/basket.module').then(mod => mod.BasketModule), data: {breadcrumb: 'Basket'}},
+  { path: 'basket', loadChildren: () => import('./basket/basket.module').then(mod => mod.BasketModule), data: {breadcrumb: 'Basket'}},
   {
     path: 'checkout',
     canActivate: [AuthGuard],
     loadChildren: () => import('./checkout/checkout.module')
     .then(mod => mod.CheckoutModule), data: {breadcrumb: 'Checkout'}},
+  {
+    path: 'orders',
+    canActivate: [AuthGuard],
+    loadChildren: () => import('./orders/orders.module')
+      .then(mod => mod.OrdersModule), data: { breadcrumb: 'Orders' }},
     {
       path: 'account', loadChildren: () => import('./account/account.module')
         .then(mod => mod.AccountModule), data: { breadcrumb: { skip: true } }
